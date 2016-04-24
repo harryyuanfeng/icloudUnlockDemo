1a963a3838f7fab9dfeb50435716b04cfb0838e1
harryfeng <harry.yuanfeng@gmail.com>
2016-04-24 15:09:16 +0800

    Initial Commit

---
+++ donggeIcloudDemoTests/donggeIcloudDemoTests.m
@@ -0,0 +1,39 @@
+//
+//  donggeIcloudDemoTests.m
+//  donggeIcloudDemoTests
+//
+//  Created by harryfeng on 4/24/16.
+//  Copyright © 2016 harryfeng. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface donggeIcloudDemoTests : XCTestCase
+
+@end
+
+@implementation donggeIcloudDemoTests
+
+- (void)setUp {
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown {
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample {
+    // This is an example of a functional test case.
+    // Use XCTAssert and related functions to verify your tests produce the correct results.
+}
+
+- (void)testPerformanceExample {
+    // This is an example of a performance test case.
+    [self measureBlock:^{
+        // Put the code you want to measure the time of here.
+    }];
+}
+
+@end
